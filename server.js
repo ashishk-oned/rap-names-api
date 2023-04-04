dda5431ba254473c84a9ff5173ea652b21c78971
ashish kumar <ashishkag.oned@gmail.com>
2023-04-04 23:39:39 +0530

    first commit -working api

--- server.js
+++ server.js
@@ -1,62 +1,82 @@
+const { request, response } = require('express');
 const express = require('express')
 const app = express()
-const cors = require('cors')
+// const cors = require('cors')
 const PORT = 8000
 
-app.use(cors())
+// app.use(cors())
 
-let rappers = {
-    '21 savage': {
-        'age': 28,
-        'birthName': 'Shéyaa Bin Abraham-Joseph',
-        'birthdate': '22 October 1992', 
-        'birthLocation': 'London, England',
-        'origin': 'Atlanta, Georgia',
-        'genre': 'hip hop, trap, rap, horrorcore',
-        'occupation': 'rapper, songwriter, record producer',
-        'yearsActive': '2013-present',
-        'labels': 'Epic, Slaughter Gang',
-        'children': 3
+// let rappers = {
+//     '21 savage': {
+//         'age': 28,
+//         'birthName': 'Shéyaa Bin Abraham-Joseph',
+//         'birthdate': '22 October 1992', 
+//         'birthLocation': 'London, England',
+//         'origin': 'Atlanta, Georgia',
+//         'genre': 'hip hop, trap, rap, horrorcore',
+//         'occupation': 'rapper, songwriter, record producer',
+//         'yearsActive': '2013-present',
+//         'labels': 'Epic, Slaughter Gang',
+//         'children': 3
+//     },
+//     'chance the rapper':{
+//         'age': 28,
+//         'birthName': 'Chancelor Jonathan Bennett',
+//         'birthdate': 'April 16, 1993', 
+//         'birthLocation': 'London, England',
+//         'origin': 'Chicago, Illinois',
+//         'genre': 'hip hop, alternative hip hop, r & b',
+//         'occupation': 'rapper, singer, song writer, record producer, activist, actor, philanthropist',
+//         'yearsActive': '2011-present',
+//         'labels': 'none',
+//         'children': 0
+//     },
+//     'unknown':{
+//         'age': 'unknown',
+//         'birthName': 'unknown',
+//         'birthdate': 'unknown', 
+//         'birthLocation': 'unknown',
+//         'origin': 'unknown',
+//         'genre': 'unknown',
+//         'occupation': 'unknown',
+//         'yearsActive': 'unknown',
+//         'labels': 'unknown',
+//         'children': 'unknown'
+//     }
+// }
+const rappers ={
+    '21 savage' : {
+        'age':29,
+        'birthName' : "Shéyaa Bin Abraham-Joseph",
+        'birthLocation': 'London, England'
     },
-    'chance the rapper':{
-        'age': 28,
+    'chance the rapper' : {
+        'age' :29,
         'birthName': 'Chancelor Jonathan Bennett',
-        'birthdate': 'April 16, 1993', 
-        'birthLocation': 'London, England',
-        'origin': 'Chicago, Illinois',
-        'genre': 'hip hop, alternative hip hop, r & b',
-        'occupation': 'rapper, singer, song writer, record producer, activist, actor, philanthropist',
-        'yearsActive': '2011-present',
-        'labels': 'none',
-        'children': 0
+        'birthLocation': 'London, England'
     },
-    'unknown':{
-        'age': 'unknown',
-        'birthName': 'unknown',
-        'birthdate': 'unknown', 
-        'birthLocation': 'unknown',
-        'origin': 'unknown',
-        'genre': 'unknown',
-        'occupation': 'unknown',
-        'yearsActive': 'unknown',
-        'labels': 'unknown',
-        'children': 'unknown'
+    'dylan': {
+        'age' : 21,
+        'birthName': 'Dylan',
+        'birthLocation':'Atlantis'
     }
-}
+
+};
 
 app.get('/', (request, response) => {
     response.sendFile(__dirname + '/index.html')
 })
 
-app.get('/api/:name', (request, response) => {
-    const rapperName = request.params.name.toLowerCase()
-    if(rappers[rapperName]){
-        response.json(rappers[rapperName])
-    }else{
-        response.json(rappers['unknown'])
-    }
+app.get('/api/:rapperName',(request,response) => {
+    // console.log (request.params.rapperName );
+    // response.json(rappers);
+    const rappersName = request.params.rapperName.toLowerCase() ;
+    if (rappers[rappersName])
+        response.json(rappers[rappersName]);
+    else 
+        response.json(rappers['dylan']);
 })
 
-app.listen(process.env.PORT || PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+app.listen( PORT, () => {
+    console.log(`Server running on port ${PORT}! You better catch it!`)
 })
